b2ec85d6a50a6ecac4b5128a1d585e9cc9b83ab4
Bahtiar Rivai <bahtiar@twiscode.com>
2018-07-10 11:17:31 +0700

    Laravel instal

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['namespace' => 'API'], function () {
+		Route::post('register', ['uses' => 'AuthController@register', 'as' => 'api.auth.getToken']);
+		Route::post('login', ['uses' => 'AuthController@login', 'as' => 'api.auth.getToken']);
+		Route::get('facebook/{token}', ['uses' => 'FacebookController@getToken', 'as' => 'api.facebook']);
+		Route::get('google/{token}', ['uses' => 'GoogleController@getToken', 'as' => 'api.google']);
+});
