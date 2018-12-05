da44b84951e778a22b1f380f6471095390ceed18
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-12-05 13:39:37 +0700

    Config twilio, registrasi failed
    - edit twilio number
    - registrasi failed, lgsg delete user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -80,19 +80,6 @@ class AuthController extends BaseController
         $remember_token     = User::whereId($user->id)->update($data);
 
         if($user){
-
-          // $code = rand(1000,9999);
-          // $user->remember_token = $code;
-          // $user->save();
-          //   try {
-          //       Nexmo::message()->send([
-          //           'to'   => $input['phone'],
-          //           'from' => 'Boxin',
-          //           'text' => 'Please use this number '.$code.' for authentication in Boxin App. Thank you.'
-          //       ]);
-          //   } catch (Nexmo\Client\Exception\Request $e) {
-          //   }
-
             return (new AuthResource($user))->additional([
                 'success' => true,
                 'message' => 'User register successfully.',
@@ -118,6 +105,7 @@ class AuthController extends BaseController
         $accountSid = config('app.twilio')['TWILIO_ACCOUNT_SID'];
         $authToken  = config('app.twilio')['TWILIO_AUTH_TOKEN'];
         $appSid     = config('app.twilio')['TWILIO_APP_SID'];
+        $number_twilio = config('app.twilio')['TWILIO_NUMBER'];
         $client     = new Client($accountSid, $authToken);
 
         $user       = User::where('id', $request->input('user_id'))->where('remember_token', $request->input('token'))->first();
@@ -138,7 +126,7 @@ class AuthController extends BaseController
                     // +919033999999, array(
                     $phone, array(
                         // A Twilio phone number you purchased at twilio.com/console
-                        'from' => '+18124322732',
+                        'from' => $number_twilio,
                         // the body of the text message you'd like to send
                         'body' => 'Please use this number '.$code.' for authentication in Boxin App. Thank you.'
                     )
@@ -154,6 +142,7 @@ class AuthController extends BaseController
             }
         } else {
             return response()->json(['message' => 'Send code failed.']);
+            User::whereId($user->id)->delete($user->id);
         }            
         
     }
@@ -190,6 +179,7 @@ class AuthController extends BaseController
         $accountSid = config('app.twilio')['TWILIO_ACCOUNT_SID'];
         $authToken  = config('app.twilio')['TWILIO_AUTH_TOKEN'];
         $appSid     = config('app.twilio')['TWILIO_APP_SID'];
+        $number_twilio = config('app.twilio')['TWILIO_NUMBER'];
         $client     = new Client($accountSid, $authToken);
 
         $phone      = $request->input('phone');
@@ -203,7 +193,7 @@ class AuthController extends BaseController
             $nomor = '+'.$phone;
             $client->messages->create(
                 $nomor, array(
-                    'from' => '+18124322732',
+                    'from' => $number_twilio,
                     'body' => 'Please use this number '.$code.' for authentication in Boxin App. Thank you.'
                 )
             );
