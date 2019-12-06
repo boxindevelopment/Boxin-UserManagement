63e978d6e4185a4f4b0e86a17c0730af820bd33a
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-12-06 11:23:05 +0700

    address error

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -22,7 +22,7 @@ class AuthResource extends Resource
             'phone' => $this->phone,
             'status' => $this->status,
             'image' => is_null($this->image) ? null : (asset('images/user').'/'.$this->image),
-            'address' => $this->address : null,
+            'address' => $this->address,
             'token' => $this->remember_token,
         ];
     }
