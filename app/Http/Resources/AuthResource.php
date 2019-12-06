7d4ced67f7fd8b89732e93845fa3de0e1a0d7e36
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-12-06 14:07:00 +0700

    with user addresses

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -22,7 +22,15 @@ class AuthResource extends Resource
             'phone' => $this->phone,
             'status' => $this->status,
             'image' => is_null($this->image) ? null : (asset('images/user').'/'.$this->image),
-            'address' => $this->address,
+            'address' => ($this->addresses) ? $this->addresses[0]->address : null,
+            'postal_code' => ($this->addresses) ? $this->addresses[0]->postal_code : null,
+            'apartment_name' => ($this->addresses) ? $this->addresses[0]->apartment_name : null,
+            'apartment_tower' => ($this->addresses) ? $this->addresses[0]->apartment_tower : null,
+            'apartment_floor' => ($this->addresses) ? $this->addresses[0]->apartment_floor : null,
+            'apartment_number' => ($this->addresses) ? $this->addresses[0]->apartment_number : null,
+            'rt' => ($this->addresses) ? $this->addresses[0]->rt : null,
+            'rw' => ($this->addresses) ? $this->addresses[0]->rw : null,
+            'village' => ($this->addresses) ? new VillageResource($this->addresses[0]->village) : null,
             'token' => $this->remember_token,
         ];
     }
